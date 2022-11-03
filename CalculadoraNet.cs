7681ee2450d54615cc4b87d62f7cc8ee4b502b85
jonatasx <jonatas.santospessoa@gmail.com>
2022-11-03 11:26:54 -0300

    OOP com classes

---
+++ CalculadoraNet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//objetivo de qualquer código é ser o mais reutilizável possível
+/*
+* NomedaEmpresa.<<a não ser que a equipe utilize ingles ai tudo
+bem- tudo em portugues>>
+* NomedaEmpresa
+*
+*/
+
+namespace Treinamento.Matematica
+{
+    /*
+* Classe é nomeada como substantivo
+* Métodos são verbos no infitivo
+* Pense em nomes bons
+*
+*/
+    class CalculadoraNet
+    {
+        public int Somar(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Treinamento.Matematica;
+
+namespace ExemploNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string xStr = Console.ReadLine();//UI
+            string yStr = Console.ReadLine();//UI
+            int x = Convert.ToInt32(xStr);
+            int y = Convert.ToInt32(yStr);
+            CalculadoraNet calc = new CalculadoraNet();
+            int r = calc.Somar(x, y);
+            Console.WriteLine(r.ToString()); //Saída
+
+            Console.ReadLine();
+        }
+
+     
+    }
+}
